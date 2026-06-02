1b519dd3dd223cb61ed244b39137cbf7c475c434
mine1712 <mcm17@correo.ugr.es>
2026-06-02 10:45:16 +0200

    mejora de estructura

---
+++ config.js
@@ -0,0 +1,25 @@
+const CONFIG = {
+    // Título principal de la web
+    title: "InfoADE",
+    subtitle: "Tu portal centralizado de asignaturas, recursos y cuestionarios",
+    
+    // Lista de asignaturas y sus módulos disponibles
+    asignaturas: [
+        {
+            id: "sibw",
+            nombre: "SIBW",
+            descripcion: "Sistemas de Información Basados en Web",
+            // Configuramos qué botones queremos que aparezcan
+            temario: false, 
+            tests: "./sibw/tests/index.html" // Ruta al index de tus tests
+        },
+        {
+            id: "dsd",
+            nombre: "DSD",
+            descripcion: "Diseño de Sistemas Distribuidos",
+            temario: "./dsd/temario/main.pdf", // Ruta directa al PDF
+            tests: false
+        }
+        // Cuando tengas una nueva asignatura, solo copias y pegas este bloque aquí abajo 👇
+    ]
+};
